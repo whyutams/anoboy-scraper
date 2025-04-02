efc71aedabb9c90e5330a1b318ac072800ba0c2e
ncrnt <wahyustamuu@gmail.com>
2025-04-02 16:40:25 +0800

    add Search & Detail anime

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 Official Website: <a href="https://ww1.anoboy.app/">anoboy</a><br>
 
 ### Anything we can do:
-| No | Path | Query | Description |
-| :---: | ------ | :------: | :--------: |
-| 1 | Home | ❌ | Info pembaruan anime |
-| <s>2</s> | <s>Search</s> | <s>✅</s> | <s>Coming soon!</s> | 
-| <s>3</s> | <s>Detail</s> | <s>✅</s> | <s>Coming soon!</s> | 
+
+| No  | Endpoint | Method |         Description          |
+| :-: | -------- | :----: | :--------------------------: |
+|  1  | Home     |   -    |     Info pembaruan anime     |
+|  2  | Search   | Query  | Cari anime berdasarkan judul |
+|  3  | Detail   |  URL   |      Info detail anime       |
 
 ### How to use:
 
@@ -18,9 +19,11 @@ const api = require("./api");
   let page = 2;
 
   api
-    .Home(page).then((result) => {
+    .Home(page)
+    .then((result) => {
       console.log(result);
-    }).catch((error) => {
+    })
+    .catch((error) => {
       console.log(error);
     });
 })();
@@ -39,27 +42,30 @@ const api = require("./api");
 })();
 ```
 
-### Result: 
+### Result:
 
 ```json
-[
-  {
-    "name": "Solo Leveling Season 2 Episode 13 Selesai",
-    "img": "https://ww1.anoboy.app/img/upload/01as-Solo%20Leveling%20Season%202.jpg",
-    "uploaded_at": { "date": "2025/03", "day": "Minggu", "hour": "00:12 Wib" },
-    "url": "https://ww1.anoboy.app/2025/03/solo-leveling-season-2-episode-13-selesai/",
-    "base_url": "https://ww1.anoboy.app"
-  },
-  {
-    "name": "Ao no Miburo Episode 24 Selesai",
-    "img": "https://ww1.anoboy.app/img/upload/01as-Ao%20no%20Miburo.jpg",
-    "uploaded_at": { "date": "2025/03", "day": "Sabtu", "hour": "22:58 Wib" },
-    "url": "https://ww1.anoboy.app/2025/03/ao-no-miburo-episode-24-selesai/",
-    "base_url": "https://ww1.anoboy.app"
-  },
+{
+  page: 2,
+  results: [
+    {
+      name: 'Solo Leveling Season 1 + 2',
+      img: 'https://ww1.anoboy.app/img/upload/01as-Solo%20Leveling.jpg',
+      uploaded_at: { date: '2024/01', day: 'Minggu', hour: '00:30 Wib' },
+      url: 'https://ww1.anoboy.app/2024/01/solo-leveling-season-1-2/',
+      base_url: 'https://ww1.anoboy.app'
+    },
+    {
+      name: 'Fate/Grand Order: Shuukyoku Tokuiten – Kani Jikan Shinden Solomon',
+      img: 'https://ww1.anoboy.apphttps://blogger.googleusercontent.com/img/a/AVvXsEhcefQ7BQyvVvFd0PWG5QDNCQnoJ0Y-TuzcZfVZCRhYu5e0Z6G4ZUBNbmAfDpsRizZRmh6AGRnPYAt2cn_MD3m_lwXFCw04btCgoGhNFg0aE0fUXHpePuqHsSv3hGHfhw-Cd6Cw0GN289U9FQ9dnvlJNPiqmsFcXQhAW00k7EEmBwQv9fwJOZ4DDD1T=s240',
+      uploaded_at: { date: '2022/02', day: 'Senin', hour: '03:28 Wib' },
+      url: 'https://ww1.anoboy.app/2022/02/fate-grand-order-shuukyoku-tokuiten-kani-jikan-shinden-solomon/',
+      base_url: 'https://ww1.anoboy.app'
+    },
 
-  ...
-]
+    ...
+  ]
+}
 ```
 
 <br><br>
--- api.js
+++ api.js
@@ -5,10 +5,10 @@ const BASE_URL = "https://ww1.anoboy.app";
 
 /**
  * Mengambil daftar anime terbaru dari halaman beranda
- * @param {number} page - nomor halaman
- * @param {boolean} not_downloadable - true/false untuk filter pembaruan download (default: false)
+ * @param {number} page - Nomor halaman
+ * @param {boolean} download_update - true/false untuk filter pembaruan download (default: false)
  */
-async function Home(page, not_downloadable = false) {
+async function Home(page, download_update = false) {
     try {
         const response = await axios.get(BASE_URL + (page ? "/page/" + page : ""));
         const $ = cheerio.load(response.data);
@@ -21,7 +21,7 @@ async function Home(page, not_downloadable = false) {
             let jamup = $(el).find(".jamup").text().split(",");
 
             let _ = {
-                name: $(el).attr("title"),
+                title: $(el).attr("title"),
                 img: `${BASE_URL}` + $(el).find("amp-img").attr("src"),
                 uploaded_at: {
                     date: url.replace(BASE_URL + "/", "").split("/").slice(0, 2).join("/"),
@@ -32,14 +32,121 @@ async function Home(page, not_downloadable = false) {
                 base_url: BASE_URL
             };
 
-            if (_.uploaded_at.day == '' || !_.uploaded_at.day || _.img == BASE_URL || not_downloadable && _.name.toLowerCase().includes("download")) return;
+            if (_.uploaded_at.day == '' || !_.uploaded_at.day || _.img == BASE_URL || !download_update && _.title.toLowerCase().includes("download")) return;
             results.push(_);
         });
 
-        return results;
+        return {
+            page: page ? page : 1,
+            results: results
+        };
     } catch (error) {
         throw new Error("Gagal fetch data Home: " + error.message);
     }
-} 
+}
 
-module.exports = { Home };
+/**
+ * Mencari anime berdasarkan query
+ * @param {string} query - Query pencarian
+ * @param {number} page - Nomor halaman
+ * @param {boolean} download_update - true/false untuk filter pembaruan download (default: false)
+ */
+async function Search(query, page, download_update = false) {
+    if (!query || query.trim().length === 0) throw new Error("Harap masukkan query!");
+
+    try {
+        const searchUrl = `${BASE_URL}/?s=${encodeURIComponent(query.split(" ").join("+"))}${page ? "/page/" + page : ""}`;
+        const response = await axios.get(searchUrl);
+        const $ = cheerio.load(response.data);
+        let results = [];
+
+        $(".container .column-content a").each((i, el) => {
+            let url = $(el).attr("href");
+            if (url.includes("page/") || url.includes("/jadwal.php") || url == "#") return;
+
+            let jamup = $(el).find(".jamup").text().split(",");
+
+            let _ = {
+                title: $(el).attr("title"),
+                img: `${BASE_URL}` + $(el).find("amp-img").attr("src"),
+                uploaded_at: {
+                    date: url.replace(BASE_URL + "/", "").split("/").slice(0, 2).join("/"),
+                    day: jamup[0].replace("UP", "").trim(),
+                    hour: jamup[1]?.trim(),
+                },
+                url: url,
+                base_url: BASE_URL
+            };
+
+            if (_.uploaded_at.day == '' || !_.uploaded_at.day || _.img == BASE_URL || !download_update && _.title.toLowerCase().includes("download")) return;
+            results.push(_);
+        });
+
+        return {
+            page: page ? page : 1,
+            results: results
+        };
+    } catch (error) {
+        throw new Error("Gagal fetch data Search: " + error.message);
+    }
+}
+
+/**
+ * Detail anime
+ * @param {string} url - link url anime 
+ */
+async function Detail(url) {
+    if (!url || url.trim().length === 0 || !isValidUrl(url)) throw new Error("Harap masukkan url yang valid!");
+
+    try {
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        let result = {};
+
+        $(".container").each((i, el) => {
+            let jamup = $(el).find("time.updated").text().split(",");
+            let column = $(el).find(".column-three-fourth");
+            let seri = [];
+
+            $(el).find(".hq").each((_, __) => {
+                seri.push($(__).text().trim());
+            });
+
+            result = {
+                pagetitle: $(el).find(".pagetitle h1").text().trim(),
+                title: $(el).find(".unduhan table tr td").eq(0).text().trim(),
+                img: column.find("amp-img").attr("src"),
+                total_eps: $(el).find(".unduhan table tr td").eq(1).text().trim().split(" ")[2] || "-",
+                total_season: seri.filter(x => x.toLowerCase().includes("season")).length || null,
+                studio: $(el).find(".unduhan table tr td").eq(2).text().trim() || "-",
+                source: $(el).find(".unduhan table tr td").eq(3).text().trim() || "-",
+                genre: $(el).find(".unduhan table tr td").eq(4).text().trim() || "-",
+                score: $(el).find(".unduhan table tr td").eq(5).text().trim() || "-",
+                synopsis: $(el).find(".unduhan").eq(0).text().trim() || "-",
+                serial: seri,
+                updated_at: {
+                    date: $(el).find("time.updated").attr("datetime").split(" ")[0],
+                    day: jamup[0].trim(),
+                    hour: jamup[1]?.trim(),
+                },
+                url: url,
+                base_url: BASE_URL
+            };
+        });
+
+        return result;
+    } catch (error) {
+        throw new Error("Gagal fetch data Search: " + error.message);
+    }
+}
+
+const isValidUrl = url => {
+    try {
+        new URL(url);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+module.exports = { Home, Search, Detail };
